88df2b51dee76e8965a284dab3455917ea846e4c
xiaoliang <xiaoliang@xiaoliang.com>
2018-08-27 17:00:35 +0800

    test1 project commit

---
+++ test1/src/com/lanou/pro/Person.java
@@ -0,0 +1,43 @@
+package com.lanou.pro;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+    public static void main(String[] args){
+        System.out.println("test person");
+        Person per=new Person("xl",18);
+        System.out.println(per);
+    }
+}
